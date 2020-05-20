f1c104c696eab6d61f68164e759644acfb1478ee
Philip Sieg <65626517+philsieg@users.noreply.github.com>
2020-05-20 19:25:58 -0400

    Andy's File

---
+++ ReadData.m
@@ -0,0 +1,9 @@
+%ReadData
+%Junior Team
+
+%This code takes as an input a csv file that contains information about the
+%object's position and velocity in the plane. The code will identify the
+%regions of the data where an impact has occurred, and will output the
+%pre-impact and post-impact set position and velocity of the object
+
+D = readtable('traj_1.csv');
