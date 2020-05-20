d3e21456789d24ac910b555514e2c7be6bd26260
philsieg <65626517+philsieg@users.noreply.github.com>
2020-05-20 11:55:27 -0400

    Add WangMason File via Upload
    File includes basic header, need to specify exactly what inputs and outputs we would like.
    No code yet.

---
+++ WangMason.m
@@ -0,0 +1,11 @@
+%% Wang Mason Impact Model
+%Authors: Andy, Joah, Natalie, Phil
+
+%Goal: 
+%Implement the Wang Mason Impact Model from their Paper "Two-Dimensional 
+%Rigid-Body Collisions With Friction". This function takes in the 
+%pre-impact state of an object and returns the post-impact velocity.
+
+%Inputs:
+
+%Outputs:
