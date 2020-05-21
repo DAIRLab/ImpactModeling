70e035915672aaa05ee05e2e1cdc85ea613447c8
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-05-21 15:45:26 +0200

    Update WangMason_Pseudocode.m

--- WangMason_Pseudocode.m
+++ WangMason_Pseudocode.m
@@ -1,2 +1,10 @@
 %This is a starting pseudocode file to write down the flow of the code
 %before we make changes to the main wang mason code file
+
+
+%define the constants B1, B2, B3: ALL are COM initial position (x1 and y1), masses, radius of gyration dependant.
+%define the constants So and Co: both are initial COM velocities dependant (ydot_1o, ....) and initial COM position dependant (x1 and y1),
+
+%figure out a way to find mu (e dependant)
+
+% 
