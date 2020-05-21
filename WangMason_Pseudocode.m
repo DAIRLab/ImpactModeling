82cf1149da4c222ec82b1fe4bab511cbc3a7a6ed
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-05-21 16:34:26 +0200

    Update WangMason_Pseudocode.m

--- WangMason_Pseudocode.m
+++ WangMason_Pseudocode.m
@@ -30,13 +30,13 @@
 
 %define e as a SYMBOL
 
-%using poisson's model, find the contact mode: P_d, P_q, and e dependant: mu, mu_s, So, Co, s, and the Bs dependant
+%using poisson's model, find the contact mode: P_d, P_q, and e dependant: mu, mu_s, So, Co, s, e, and the Bs dependant
 % ^ page 639 table 1
 
 % ^ since we do not know e, we won't actually know what contact mode we are experiencing
 % so, we define all the the resulting impulses Px and Py (in terms of e) for all contact modes and find for each contact mode
 % the e that minimizes the metric error. and the contact mode with the e that results in the smallest metric error
-%is the correct and most realistic contact mode.
+%is the correct e with the most realistic contact mode.
 % Since the Poisson Hypothesis of restitution says that the coefficient of restitution e is ONLY MATERIAL DEPENDANT, If we run
 %this section multiple times for multiple trials (like what was done in paper 1, they "fit" the models to find the most
 % realistic e and mu that fit 80 trials) we can find an e that is reasonable (or maybe find e values for all trials and then 
@@ -46,12 +46,8 @@
 
 
 %so here after the previuos section, we have an e that minimizes the metric error on average for a good number of trials
-%and then we can use that e to get the most realistic Px and Py.
+%and then we can use that e to get the most realistic Px and Py given an input of preimpact velocities and positions.
 % but all of this is mu dependant and I haven't thought of how to approach that yet.
 
-%de
-
-
-
-
-
+%find the post impact velocities (xdot_1 and ...)
+% ^ equations 5-7
