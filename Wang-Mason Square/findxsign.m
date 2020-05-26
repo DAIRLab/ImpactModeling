da09ac1a11cb5ae232096fc0dc1b4496c8c063b6
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-05-26 22:10:59 +0200

    Update findxsign.m

--- Wang-Mason Square/findxsign.m
+++ Wang-Mason Square/findxsign.m
@@ -10,18 +10,18 @@ sign =0;
 if theta1>0 %turning anticlockwise
     r = rem(theta1,pi)/pi;
     if r >0 && r<0.25 || r >0.5 && r<0.75 %angle between 0 to 90 degrees and 180-270
-        sign = 1; %x1 is to the right of contact point
+        sign = -1; %x1 is to the right of contact point
     elseif r >0.25 && r<0.5 || r>0.75 %angle between 90 and 180 and 270-360
-        sign = -1; %x1 will be on the left
+        sign = 1; %x1 will be on the left
     else %when it is right on top
         sign = 1;
     end
 elseif theta1 <0 %turning clockwise
         r = rem(abs(theta1),pi)/pi;
     if r >0 && r<0.25 || r >0.5 && r<0.75 %angle between 0 to 90 degrees and 180-270
-        sign = -1; %x1 is to the left of contact point
+        sign = 1; %x1 is to the left of contact point
     elseif r >0.25 && r<0.5 || r>0.75 %angle between 90 and 180 and 270-360
-        sign = 1; %x1 will be on the right
+        sign = -1; %x1 will be on the right
     else %when it is right on top
         sign = 1;
     end
