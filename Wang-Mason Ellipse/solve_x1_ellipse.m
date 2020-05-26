eb2334cb936e6705a49d49c44cd7f304c8ea6bee
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-05-26 22:19:18 +0200

    Update solve_x1_ellipse.m

--- Wang-Mason Ellipse/solve_x1_ellipse.m
+++ Wang-Mason Ellipse/solve_x1_ellipse.m
@@ -8,10 +8,11 @@ x1eq = solve(eq1, sym('x'));
 
 a0 = 0.07/2; %semi axis major
 b0 = 0.05/2; %semi axis minor
-h0 = 0; %h0 and k0 are 0 since we assume the com is the origin
+h0 = 0; %h0 and k0 are 0 since we assume the com is the origin of the ellipse
 k0 = 0;
 th0 = theta1; %tilting angle
-y0= y1; %distance from com to contact point
+y0= -y1; %distance from com to contact point (going down from com to contact point hence the negative sign) because y1 
+%goes up from the table ie contact point to COM (
 
 %solving for x1v
 x1 = subs(x1eq, [a,b,h,k,th,y], [a0,b0,h0,k0,th0,y0]); %gives out the x distance from the COM to the contact point in both positive
