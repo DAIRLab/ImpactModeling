f35d7636d93e869b8d0b288b03a843919224ae1c
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-05-26 22:04:49 +0200

    Update ErrorEllipse.m

--- Wang-Mason Ellipse/ErrorEllipse.m
+++ Wang-Mason Ellipse/ErrorEllipse.m
@@ -81,12 +81,24 @@ else
 end
 
 %use a tilted ellipse equation to find x1 (x distance between the COM and the contact point)
-%g is the half the minor axis
-%h is half the major axis
 
-eq1 = (((x)*cos(theta1) + (y1)*sin(theta1))^2)/g^2 + (((x)*sin(theta1) - (y1)*cos(theta1))^2)/h^2 == 1;
-x1 = solve(eq1, sym('x'));
-%use the signx1 script above to pick a positive or negative x1 from the solution above
+syms x y h k a b th
+
+eq1 = (((x-h)*cos(th) + (y-k)*sin(th))^2)/a^2 + (((x-h)*sin(th) - (y-k)*cos(th))^2)/b^2 == 1;
+x1eq = solve(eq1, sym('x'));
+
+
+a0 = 0.07/2; %semi axis major
+b0 = 0.05/2; %semi axis minor
+h0 = 0; %h0 and k0 are 0 since we assume the com is the origin
+k0 = 0;
+th0 = theta1; %tilting angle
+y0= y1; %distance from com to contact point
+
+%solving for x1v
+x1 = subs(x1eq, [a,b,h,k,th,y], [a0,b0,h0,k0,th0,y0]); %gives out the x distance from the COM to the contact point in both positive
+%negative since the answer is a square root
+
 
 x1dot_0 = pre(1,4);
 y1dot_0 = pre(1,5);
