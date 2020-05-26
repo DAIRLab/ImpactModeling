b59942494a457d7889ae758a0ff2b9a0bf6ccbaa
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-05-26 22:02:26 +0200

    Update solve_x1_ellipse.m

--- Wang-Mason Ellipse/solve_x1_ellipse.m
+++ Wang-Mason Ellipse/solve_x1_ellipse.m
@@ -9,13 +9,15 @@ x1 = solve(eq1, sym('x'));
 x1_2 = solve(eq1_2, sym('x'));
 
 
-a0 = 0.07/2; %since the 
-b0 = 0.05/2;
-h0 = 0;
+a0 = 0.07/2; %semi axis major
+b0 = 0.05/2; %semi axis minor
+h0 = 0; %h0 and k0 are 0 since we assume the com is the origin
 k0 = 0;
-th0 = theta1;
-y0= y1;
+th0 = theta1; %tilting angle
+y0= y1; %distance from com to contact point
 
 %solving for x1v
-x1v = subs(x1, [a,b,h,k,th,y], [a0,b0,h0,k0,th0,y0]);
+x1v = subs(x1, [a,b,h,k,th,y], [a0,b0,h0,k0,th0,y0]); %gives out the x distance from the COM to the contact point in both positive
+%negative since the answer is a square root
 
+ 
