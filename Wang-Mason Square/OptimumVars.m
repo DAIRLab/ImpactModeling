a37c5d5f1e1cf7ab10ff3bcb99f72953a124cf59
joahkim00 <65670834+joahkim00@users.noreply.github.com>
2020-05-26 14:20:35 -0400

    Add files via upload

---
+++ Wang-Mason Square/OptimumVars.m
@@ -0,0 +1,33 @@
+% This code calulates the average best mu and epsilon for a number of
+% trials. We can have the compiled code error function output if the
+% trial is a sticking trial and the min mu for that specific trial. We 
+% then have this code select the max mu for all sticking trials and weight
+% that mu with the number of sticking trials, then go on and do an average
+% of the mus and epsilons normally
+
+function [OptMu, OptEps] = OptimumVars
+ran = randi([1 500],1,80);
+MuVec = zeros(1,80);
+EpsVec = zeros(1,80);
+MuStickVec = [];
+stickcount = 0;
+for z = 1:80
+    n = ran(z);
+    [stick,Mu,Ep] = Error(n); %we can eedit output of this code
+    if stick == 1
+        MuStickVec(end+1) = Mu;
+        sickcount = stickcount+1;
+        EpsVec(z) = Ep;
+    else
+        MuVec(z) = Mu;
+        EpsVec(z) = Ep;
+    end
+end
+maxstickMu = max(MuStickVec);
+MuStick_w = maxstickMu*stickcount; %weigh the value of the max mu of the sticking trials
+[i,j] = size(MuVec);
+OptMu = (sum(MuVec)+MuStick_w)/(j+stickcount);
+
+[i,j] = size(EpsVec);
+OptEps = sum(EpsVec)/j;
+end
---
+++ Wang-Mason Square/readImpactData.m
@@ -0,0 +1,32 @@
+% This function takes in the specified .csv file and outputs the pre and
+% post impact states of the first impact
+% INPUTS: csvFile - name of the .csv file to be accessed
+% OUTPUTS: pre - vector of pre impact state of first collision
+%          post - vector of post impact state of first collision
+function [pre, post] = readImpactData(csvFile) 
+
+    %load data
+    traj = readtable(csvFile);
+    
+    %convert from table to matrix
+    trajectory = traj{:, :};
+    
+    %initial impact matrix 
+    impacts = zeros(1, 6, 2);
+
+    %iterator variable to keep track of impact #
+    curr = 1; 
+    
+    %loop through all of data 
+    for i = 2:length(trajectory)-1
+        if (trajectory(i-1,5) < 0) && (trajectory(i,5) > 0)
+            impacts(curr, :, 1) = trajectory(i-1,:);  %preimpact data (first array)
+            impacts(curr, :, 2) = trajectory(i,:);    %post impact data (second array)
+            curr = curr + 1; 
+        end
+    end
+    
+    pre = impacts(1,:,1);
+    post = impacts(1,:,2);
+    
+end
