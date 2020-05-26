9b40d9e725f13fa0a1dcb0f7b447cb1c7f385de4
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-05-26 21:56:18 +0200

    Update solve_x1_ellipse.m

--- Wang-Mason Ellipse/solve_x1_ellipse.m
+++ Wang-Mason Ellipse/solve_x1_ellipse.m
@@ -8,6 +8,14 @@ eq1_2 = (((x)*cos(th) + (y)*sin(th))^2)/a^2 + (((x)*sin(th) - (y)*cos(th))^2)/b^
 x1 = solve(eq1, sym('x'));
 x1_2 = solve(eq1_2, sym('x'));
 
+
+a0 = 0.07/2; %since the 
+b0 = 0.05/2;
+h0 = 0;
+k0 = 0;
+th0 = theta1;
+y0= y1;
+
 %solving for x1v
-x1v = subs(x1, [a,b,h,k,th,y], [0.07,0.05,h0,k0,th0,y0]);
+x1v = subs(x1, [a,b,h,k,th,y], [a0,b0,h0,k0,th0,y0]);
 
