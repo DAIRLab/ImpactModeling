428e19b84cf5a5e49d19f062764e4330870ca72a
andyeske <65673243+andyeske@users.noreply.github.com>
2020-05-27 14:44:08 -0300

    Update OptimumVarsEllipse.m

--- Wang-Mason Ellipse/OptimumVarsEllipse.m
+++ Wang-Mason Ellipse/OptimumVarsEllipse.m
@@ -1,19 +1,21 @@
-% This code calulates the average best mu and epsilon for a number of
-% trials. We can have the compiled code error function output if the
-% trial is a sticking trial and the min mu for that specific trial. We 
-% then have this code select the max mu for all sticking trials and weight
-% that mu with the number of sticking trials, then go on and do an average
-% of the mus and epsilons normally
-
-function [OptMu, OptEps] = OptimumVarsEllipse
-ran = randi([1 2000],1,80);
+ran = randi([1 2000],1,500);
 MuVec = [];
-EpsVec = zeros(1,80);
+EpsVec = zeros(1,500);
 MuStickVec = [];
 stickcount = 0;
-for z = 1:80
+
+% access actual data of first trajectory
+load('ellipse_uniform.mat');
+
+
+for z = 1:500
     n = ran(z);
-    [stick,Mu,Ep] = ErrorEllipse(n); %we can eedit output of this code
+    % pre - vector of pre impact state [x1_0, y1_0, theta1_0, x1dot_0, y1dot_0, theta1dot_0]
+    % post - vector of post impact state [x1_act, y1_act, theta1_act, x1dot_act, y1dot_act, thetadot_act]
+    pre = bounce_array(n).states(1:6); 
+    post = bounce_array(n).states(7:12);
+    [stick,Mu,Ep] = ErrorEllipse(n,pre,post); %we can eedit output of this code
+    
     if stick == 1
         MuStickVec(end+1) = Mu;
         stickcount = stickcount+1;
