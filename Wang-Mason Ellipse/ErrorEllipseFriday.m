6b646d910d12deede35b3e3400e5045e382782be
andyeske <65673243+andyeske@users.noreply.github.com>
2020-05-29 14:14:30 -0300

    Update ErrorEllipseFriday.m

--- Wang-Mason Ellipse/ErrorEllipseFriday.m
+++ Wang-Mason Ellipse/ErrorEllipseFriday.m
@@ -46,53 +46,13 @@ errors = zeros(sz,sz); %size based on using intervals that exclude 0 and 1
 theta1 = pre(1,3);
 y1 = J(2,3);
 x1 = J(1,3);
-
-% %find the sign of x1, ie whether it is on the left or right of the contact point.
-% signx1 = 0;
-% if theta1 > 0 %turning anticlockwise
-%     r1= rem(theta1,pi)/pi;
-%     if r > 0 && r < 0.25 || r > 0.5 && r < 0.75 %angle between 0 to 90 degrees and 180-270
-%         signx1 = 1; %x1 is to the right of contact point
-%     elseif r > 0.25 && r < 0.5 || r > 0.75 %angle between 90 and 180 and 270-360
-%         signx1 = -1; %x1 will be on the left
-%     else %when it is right on top
-%         signx1 = 1;
-%     end
-% elseif theta1 < 0 %turning clockwise
-%     r = rem(abs(theta1),pi)/pi;
-%     if r > 0 && r < 0.25 || r > 0.5 && r < 0.75 %angle between 0 to 90 degrees and 180-270
-%         signx1 = -1; %x1 is to the left of contact point
-%     elseif r > 0.25 && r < 0.5 || r > 0.75 %angle between 90 and 180 and 270-360
-%         signx1 = 1; %x1 will be on the right
-%     else %when it is right on top
-%         signx1 = 1;
-%     end
-% elseif theta1 == 0 %doesn't even rotate
-%     signx1 = 1;
-% else
-%     signx1 = 1; 
-% end
-% 
-% %use a tilted ellipse equation to find x1 (x distance between the COM and the contact point)
-% a = a0;
-% b = b0;
-% th = theta1;
-% k = 0;
-% h = 0;
-% y = -y1;
-% 
-% x1 = double((a*b*(b^2*cos(th)^2 - k^2*cos(th)^4 + a^2*sin(th)^2 - y^2*cos(th)^4 - k^2*sin(th)^4 - y^2*sin(th)^4 + 2*k*y*cos(th)^4 + 2*k*y*sin(th)^4 - 2*k^2*cos(th)^2*sin(th)^2 - 2*y^2*cos(th)^2*sin(th)^2 + 4*k*y*cos(th)^2*sin(th)^2)^(1/2) + b^2*h*cos(th)^2 + a^2*h*sin(th)^2 - a^2*k*cos(th)*sin(th) + b^2*k*cos(th)*sin(th) + a^2*y*cos(th)*sin(th) - b^2*y*cos(th)*sin(th))/(a^2*sin(th)^2 + b^2*cos(th)^2));
-%  
-% x1 = signx1*abs(x1(1)); %since we figured out the sign of x1 above in the if statement, we multiply it by the the absolute of the 
-%value of the first elemtent of the x1 vector. (it doesn't matter whether we choose the first or second elements because they
-%have the same value just different signs
-
 x1dot_0 = pre(1,4);
 y1dot_0 = pre(1,5);
 theta1dot_0 = pre(1,6);
 
 %assign pre impact velocities for object 2
-x2 = 1; %these are arbitrary since the velocities are 0 anyway
+%these are arbitrary since the velocities are 0 anyway
+x2 = 1; 
 y2 = 1;
 theta2 = 1;
 x2dot_0 = 0;
@@ -119,7 +79,6 @@ if (S_0 ~= 0)
 else
     s = 1;
 end
-
 
 %% use error metric to determine best mu and e
 % 1. compute error of the matrix containing Px & Py values by comparing to actual data
@@ -148,21 +107,25 @@ for a = 1:sz %varying mu from [0, 1]
         
             %R (Third Row of Table)
         elseif (Pq < Pd) && (Pd < (1+e) * Pq)
-            if u > abs(u_s) %R-Sticking
+            %R-Sticking
+            if u > abs(u_s) 
                 Py = -(1+e)* C_0 /(B2 + s * u * B3);  %(44)
                 Px = (B3*Py - S_0) / B1;              %(43)
-
-            else %R-Reversed Sliding
+            %R-Reversed Sliding
+            else 
                 Py = -(1+e) * C_0/(B2 + s * u * B3);              %(48)
                 Px = s * u * (Py - 2 * S_0 / (B3 + s * u * B1));  %(47)
             end
             
         %C (Fourth Row of Table)
+        
         elseif (Pd < Pq)
-            if u > abs(u_s) %C-Sticking
+            %C-Sticking
+            if u > abs(u_s) 
                  Py = -(1+e) * (B1 * C_0 + B3 * S_0)/(B1*B2 - B3^2); %(42)
                  Px = (B3 * Py - S_0)/B1;                            %(41)
-            else %C-Reversed Sliding
+           %C-Reversed Sliding
+           else 
                  Py = -(1+e)/(B2 - s * u * B3)* (C_0 + ...
                      (2*s*u*B3*S_0)/(B3 + s * u * B1));               %(46)
                  Px = s * u *(Py - 2 * S_0 / (B3 + s * u * B1));    %(45)
@@ -171,8 +134,7 @@ for a = 1:sz %varying mu from [0, 1]
             disp("Error, none of contact mode requirements met");
         end    
 
-       % calculate the post impact velocities according to the contact
-        % mode
+       % calculate the post impact velocities according to the contact mode
         x1dot_calc = Px/m1 + pre(1,4); 
         y1dot_calc = Py/m1 + pre(1,5); 
         V_cpost = J*[post(1,4);post(1,5);post(1,6)];
