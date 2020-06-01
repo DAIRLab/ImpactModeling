6cea65a86a3df2e5b6d686950938d24d6b5dd7c7
andyeske <65673243+andyeske@users.noreply.github.com>
2020-06-01 13:54:33 -0300

    Update ErrorEllipseFriday.m

--- Wang-Mason Ellipse/ErrorEllipseFriday.m
+++ Wang-Mason Ellipse/ErrorEllipseFriday.m
@@ -1,6 +1,6 @@
 % This function calculates the best Mu and best Epsilon for one trial
 
-function [stick, bestMu, bestEpsilon] = ErrorEllipseFriday(n,pre,post,J)
+function [stick, error, bestMu, bestEpsilon] = ErrorEllipseFriday(n,pre,post,J)
 
 %%Find Contact Mode and Apply Equations
 %Authors: Joah, Phil, Natalie, Andy
