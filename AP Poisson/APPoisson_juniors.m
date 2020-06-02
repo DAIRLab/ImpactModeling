d0650fe578dfc07f412b85379306737b1b5c8501
Philip Sieg <65626517+philsieg@users.noreply.github.com>
2020-06-02 14:01:12 -0400

    Create files/comment

---
+++ AP Poisson/APPoisson_juniors.m
@@ -0,0 +1,18 @@
+%% AP Poisson Model
+%From Formulating Dynamic Multi-Rigid-Body Contact Problems with Friction 
+% as Solvable Linear Complementarity Problems
+% M. ANITESCU
+% F. A. POTRA
+
+%This problems solves the LCP to resolve post impact states given the
+%preimpact states as well as information about the objects geometry
+
+% Inputs
+
+
+%Outpts
+
+function [v_post] = APPoisson_juniors(v_in)
+
+
+end
