00a23d5c71b0992951debfc160dcc182cc164ed1
Philip Sieg <65626517+philsieg@users.noreply.github.com>
2020-06-02 17:44:12 -0400

    Adding inputs and some variables

--- AP Poisson/APPoisson_juniors.m
+++ AP Poisson/APPoisson_juniors.m
@@ -8,11 +8,38 @@
 %preimpact states as well as information about the objects geometry
 
 % Inputs
+% M - 
+% d - tangential Jacobian (from data set)
+% n - normal Jacobian (from data set) 
+% v_pre - intial velocity of body including xdot, ydot, and thetadot
+% mu - coefficiant of friction (Coulumbic Friction)
+% epsilon - coefficiant of resitution
 
 
 %Outpts
+%v_post - the post impact velocity 
 
-function [v_post] = APPoisson_juniors(v_in)
+
+function [v_post] = APPoisson_juniors(M, n, d, v_pre, mu, epsilon)
+%--- Set up variables ----
+%number of edges of the polygonal approximation
+u = 2; % two dimensional problem 
+e = ones(2,1); %vectors of ones
+D = [-d, d];
+
+
+
+
+
+
+
+
+
+
+%Compression Phase
+
+%Decompression Phase
+
 
 
 end
