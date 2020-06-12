1aee376f3261f6ee1f3ec33a64397d6634d74725
andyeske <65673243+andyeske@users.noreply.github.com>
2020-06-12 18:52:28 -0300

    Update OptimumPhilModified.m

--- Comparing Wang and AP/OptimumPhilModified.m
+++ Comparing Wang and AP/OptimumPhilModified.m
@@ -76,13 +76,6 @@ minError = min(min(averageError));  %get the minimum error
 bestEp = sample(a);
 bestMu = sample(b);
 
-%create contour plot
-%colormap(flipud(gray))  %match color from Nima paper
-%contourf(sample(1:end), sample(1:end), averageError(1:end, 1:end)', 30);
-%colorbar('Direction','reverse'); 
-%xlabel("Epsilon")
-%ylabel("Mu")
-
 %Finding average mu and ep by taking the average of the best of each trial
 aa = mean(bev);
 bb = mean(bmv);
@@ -91,12 +84,20 @@ bestEp2 = sample(floor(bb)) + (sample(floor(bb)+1)-sample(floor(bb)))/(1)*(bb-fl
 bestMu2 = sample(floor(aa)) + (sample(floor(aa)+1)-sample(floor(aa)))/(1)*(aa-floor(aa)) 
 
 %Analysis of Pre-impact cases
-vm = find(v(bmv == 1));
-vm2 = find(v(bmv ~= 1));
+vm = v(bmv == 1);
+vm2 = v(bmv ~= 1);
 l = length(vm);
 l2 = length(vm2);
 Matrix = zeros(l,7);
 Matrix2 = zeros(l2,7);
+
+%Creating dummy variables - these store the coordinates of the places where 
+%the radius is close to 0.025 or 0.035
+a = [];
+a2 = [];
+b = [];
+b2 = [];
+%This information is processed at the very bottom of this code
 
 disp('Statistics for pre-impact data for minimum mu')
 for kk = 1:l  
@@ -104,7 +105,13 @@ for kk = 1:l
   d = (bounce_array(vm(kk)).d);   %tangential
   n = (bounce_array(vm(kk)).n);   %normal    
   J = [d;n];
-  Matrix(kk,7) = sqrt(J(2,3)^2+J(1,3)^2);
+  r1 = sqrt(J(2,3)^2+J(1,3)^2);
+  Matrix(kk,7) = r1;
+  if r1 < 0.026
+      a = [a, vm(kk)];
+  elseif r1 > 0.034
+      b = [b, vm(kk)];
+  end
 end
 avg = mean(Matrix)
 standard = std(Matrix)
@@ -116,7 +123,13 @@ for jj = 1:l2
     d2 = (bounce_array(vm2(jj)).d);   %tangential
     n2 = (bounce_array(vm2(jj)).n);   %normal    
     J2 = [d2;n2];
-    Matrix2(jj,7) = sqrt(J2(2,3)^2+J2(1,3)^2);
+    r2 = sqrt(J2(2,3)^2+J2(1,3)^2);
+    Matrix2(jj,7) = r2;
+    if r2 < 0.026
+      a2 = [a2, vm2(jj)];
+  elseif r2 > 0.034
+      b2 = [b2, vm2(jj)];
+  end
 end
 avg2 = mean(Matrix2)
 standard2 = std(Matrix2)
@@ -143,3 +156,49 @@ title('Radii for pre-impact data for minimum mu')
 nexttile
 histogram(Matrix2(:,7),40)
 title('Radii for pre-impact data for all other mu')
+
+%The following matrices contain some very useful information - they store
+%the impact data of those impacts whose radii are very close to matching
+%that of one of the major axis
+ll1 = length(a);
+M1 = zeros(ll1,6);
+for hh1 = 1:ll1
+    M1(hh1,1:6) = bounce_array(a(hh1)).states(1:6); 
+    vect1 = M1(:,3)*180/pi;
+end
+
+ll2 = length(b);
+M2 = zeros(ll2,6);
+for hh2 = 1:ll2
+    M2(hh2,1:6) = bounce_array(b(hh2)).states(1:6); 
+    vect2 = M2(:,3)*180/pi;
+end
+
+ll3 = length(a2);
+M3 = zeros(ll3,6);
+for hh3 = 1:ll3
+    M3(hh3,1:6) = bounce_array(a2(hh3)).states(1:6); 
+    vect3 = M3(:,3)*180/pi;
+end
+
+ll4 = length(b2);
+M4 = zeros(ll4,6);
+for hh4 = 1:ll4
+    M4(hh4,1:6) = bounce_array(b2(hh4)).states(1:6);
+    vect4 = M4(:,3)*180/pi;
+end
+
+%Creating a figure where we can display all of this
+tiledlayout(2,2) % Requires R2019b or later
+nexttile
+histogram(vect1,48)
+title('Angles for pre-impact data for minimum mu when the radius < 0.026')
+nexttile
+histogram(vect2,48)
+title('Angles for pre-impact data for minimum mu when the radius > 0.034')
+nexttile
+histogram(vect3,48)
+title('Angles for pre-impact data for all other mu when the radius < 0.026')
+nexttile
+histogram(vect4,48)
+title('Angles for pre-impact data for all other mu when the radius > 0.034')
