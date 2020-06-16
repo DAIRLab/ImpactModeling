1820832a723c36ca33d37881931168860ecd428f
andyeske <65673243+andyeske@users.noreply.github.com>
2020-06-15 21:02:27 -0300

    Update IRB_bound.m

--- IRB/IRB_bound.m
+++ IRB/IRB_bound.m
@@ -42,8 +42,8 @@ J = [d;n];
 Initial = 0.5*pre'*M*pre;
 
 % Applying fmincon
-fun = @(x)norm(post - (pre + (M^-1)*J'*[x(1);x(2)]));
-nonlcon = @(x) IRB(x, trial);
+fun = @(P)norm(post - (pre + (M^-1)*J'*[P(1);P(2)]));
+nonlcon = @(P) IRB(P, trial);
 x0 = [0 0];
 A = []; % No other constraints
 b = [];
@@ -53,11 +53,26 @@ lb = [];
 ub = [];
 % output: x(1) - tangential impulse
 %         x(2) - normal impulse
- x = fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon)
+P = fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon)
 
+%Plotting the energy ellipse
+syms x y
+assume(x >= 0)
+assume(y >= 0)
+p = 0.5*(pre + (M^-1)*J'*[x;y])'*M*(pre + (M^-1)*J'*[x;y]) - 0.5*pre'*M*pre;
+a = expand(p)
+ezplot(a, [-0.1 0.1 -0.02 0.23])
+hold on 
+plot([-0.1 0.1],[0 0],'b')
+plot([0 0],[-0.02 0.23],'b')
+title({'Energy Ellipse plot for trial #' num2str(trial)},'FontSize',15)
+annotation('textbox',[.515 .7 .1 .2],'String','Py','EdgeColor','none','FontSize',15)
+annotation('textbox',[.85 0.03 .1 .2],'String','Px','EdgeColor','none','FontSize',15)
+hold on
+plot(P(1),P(2),'*','MarkerSize',20,'linewidth',2)
 
 % output: c - constraint (energy cannot increase)
-function [c,ceq] = IRB(x, trial)
+function [c,ceq] = IRB(P, trial)
 load('ellipse_uniform.mat');
 % Set up Constants
 a0 = 0.07/2; %semi-major axis
@@ -77,6 +92,6 @@ n = (bounce_array(trial).n);   %normal
 
 J = [d;n];
 
-c(1) = 0.5*(pre + (M^-1)*J'*[x(1);x(2)])'*M*(pre + (M^-1)*J'*[x(1);x(2)]) - 0.5*pre'*M*pre;
+c(1) = 0.5*(pre + (M^-1)*J'*[P(1);P(2)])'*M*(pre + (M^-1)*J'*[P(1);P(2)]) - 0.5*pre'*M*pre;
 ceq = [];
 end
