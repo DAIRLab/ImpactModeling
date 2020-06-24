f5c683513385929b38af8f26b401cddfcd59709f
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-06-24 20:12:18 +0200

    Add files via upload

---
+++ IRB Bound/Width Vs Norm Error plot - width as input to optimization - no cap (No max width capabilities)/constraintwidth.m
@@ -0,0 +1,25 @@
+%% Constraints function 
+
+%Inputs:
+
+%P - 2x1 vector of impulses (Pn and Pt)
+%M - generalized mass matrix
+%J - jacobian
+%v_pre - pre impact velocity
+%v_post - post impact velocity 
+
+%Outputs
+%c - constraint equations, written in <= 0 form
+
+
+function [c, ceq] = constraint(P, M, J, v_pre, v_post,width)
+    %impact law prediction (ILP) for post impact velocity
+    ILP = v_pre + inv(M) * J' * [P P(2)*width]'; 
+    %initial energy from pre impact velocity 
+    initialEnergy = 1/2 * v_pre' * M * v_pre;
+    %Simplified Energy Ellipse Equaion, written in terms of <= 0
+    c(1) = 1/2 * ILP' * M * ILP - initialEnergy; %TODO
+    
+    ceq = [];
+
+end
