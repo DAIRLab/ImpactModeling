59f266c4aa7fe22115f0e0ed39f4c2771ffdd3bd
natalieaziz <49778905+natalieaziz@users.noreply.github.com>
2020-06-24 20:20:15 +0200

    Add files via upload

---
+++ IRB Bound/IRB Torque - with cap (with max error capabilities)/constraint.m
@@ -0,0 +1,26 @@
+%% Constraints function 
+
+%Inputs:
+
+%P - 2x1 vector of impulses (Pn and Pt)
+%M - generalized mass matrix
+%J - jacobian
+%v_pre - pre impact velocity
+%v_post - post impact velocity 
+
+%Outputs
+%c - constraint equations, written in <= 0 form
+
+
+function [c, ceq] = constraint(P, M, J, maxWidth, v_pre, v_post)
+    %impact law prediction (ILP) for post impact velocity
+    ILP = v_pre + inv(M) * J' * [P(1:2)'; P(3) * P(2)]; 
+    %initial energy from pre impact velocity 
+    initialEnergy = 1/2 * v_pre' * M * v_pre;
+    %Simplified Energy Ellipse Equaion, written in terms of <= 0
+    c(1) = 1/2 * ILP' * M * ILP - initialEnergy; 
+    %max width constraint, written in terms of <=
+    c(2) = abs(P(3)) - maxWidth;
+    ceq = [];
+
+end
