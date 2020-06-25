48091efca461a3a0cd8aee59856772826baedccf
joahkim00 <65670834+joahkim00@users.noreply.github.com>
2020-06-25 13:55:49 -0400

    Update plotCorrelations.m

--- IRB Bound/Torque Analysis/plotCorrelations.m
+++ IRB Bound/Torque Analysis/plotCorrelations.m
@@ -27,6 +27,7 @@ for i = 1:numTrials
     % Finding pre and post impact velocities / states
     pre = bounce_array(z).states(4:6)';
     post = bounce_array(z).states(10:12)';
+    yPos = bounce_array(z).states(2);
 
     d = (bounce_array(z).d);   %tangential
     n = (bounce_array(z).n);   %normal
@@ -52,7 +53,7 @@ for i = 1:numTrials
     P_n = [P_n; P(2)];
     P_t = [P_t; P(1)];
     moment_n = [moment_n; P(3)];
-    moment_t = [moment_t; P(1)*250*pre(2)]; %this is assuming that the pre-impact y coordinate is the y-coordinate of the COM at impact **WILL NOT give highly accurate results
+    moment_t = [moment_t; P(1)*yPos]; %this is assuming that the pre-impact y coordinate is the y-coordinate of the COM at impact **WILL NOT give highly accurate results
     moment_net = [moment_net; moment_n(end) + moment_t(end)];
     count = count + 1;
     
@@ -60,43 +61,67 @@ for i = 1:numTrials
 end
 
 figure(1)
-scatter(1000*width, errors,'filled')
+scatter(1000*width, errors,'filled', 'b')
 title("Patch Size vs Error for " + count + " Random Trials")
 xlabel('Width [mm]')
 ylabel('Normalized Error')
 
 figure(2)
-scatter(1000*moment_n, P_n, 'filled')
+scatter(1000*moment_n, P_n, 'filled', 'b')
+hold on
+line([-5 5],[0 0], 'Color', 'black'); 
+hold on
+line([0 0], [0 0.2], 'Color', 'black');
 title("Moment due to Normal Impulse vs Normal Impulse for " + count + " Random Trials")
 xlabel('Moment (Normal) [Nmm]')
 ylabel('Normal Impulse [N*s]')
 
 figure(3)
-scatter(1000*moment_n, P_t, 'filled')
+scatter(1000*moment_n, P_t, 'filled', 'b')
+hold on
+line([-5 5],[0 0], 'Color', 'black'); 
+hold on
+line([0 0], [-.05 0.03], 'Color', 'black');
 title("Moment due to Normal Impulse vs Tangential Impulse for " + count + " Random Trials")
 xlabel("Moment (Normal) [Nmm]")
 ylabel("Tangential Impulse [N*s]")
 
 figure(4)
-scatter(moment_t, P_n, 'filled')
+scatter(1000*moment_t, P_n, 'filled','b')
+hold on
+line([-80 100],[0 0], 'Color', 'black');
+hold on
+line([0 0], [0 0.2], 'Color', 'black');
 title("Moment due to Tangential Impulse vs Normal Impulse for " + count + " Random Trials")
-xlabel("Moment (Tangential) [Nm]")
+xlabel("Moment (Tangential) [Nmm]")
 ylabel("Normal Impulse [N*s]")
 
 figure(5)
-scatter(moment_t, P_t, 'filled')
+scatter(1000*moment_t, P_t, 'filled', 'b')
+hold on
+line([-80 100],[0 0], 'Color', 'black'); 
+hold on
+line([0 0], [-.05 0.03], 'Color', 'black');
 title("Moment due to Tangential Impulse vs Tangential Impulse for " + count + " Random Trials")
-xlabel("Moment (Tangential) [Nm]")
+xlabel("Moment (Tangential) [Nmm]")
 ylabel("Tangential Impulse [N*s]")
 
 figure(6)
-scatter(moment_net, P_n, 'filled')
+scatter(1000*moment_net, P_n, 'filled', 'b')
+hold on
+line([-80 120],[0 0], 'Color', 'black'); 
+hold on
+line([0 0], [0 0.2], 'Color', 'black');
 title("Net Moment vs Normal Impulse for " + count + " Random Trials")
-xlabel("Net Moment [Nm]")
+xlabel("Net Moment [Nmm]")
 ylabel("Normal Impulse [N*s]")
 
 figure(7)
-scatter(moment_net, P_t, 'filled')
+scatter(1000*moment_net, P_t, 'filled', 'b')
+hold on
+line([-80 120],[0 0], 'Color', 'black'); 
+hold on
+line([0 0], [-.05 0.03], 'Color', 'black');
 title("Net Moment vs Tangential Impulse for " + count + " Random Trials")
-xlabel("Net Moment [Nm]")
+xlabel("Net Moment [Nmm]")
 ylabel("Tangential Impulse [N*s]")
