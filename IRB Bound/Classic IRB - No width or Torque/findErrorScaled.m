b7806386aa92a8f58cd01aabd04e197910134e6b
Philip Sieg <65626517+philsieg@users.noreply.github.com>
2020-07-21 15:41:56 -0400

    small changes

--- IRB Bound/Classic IRB - No width or Torque/findErrorScaled.m
+++ IRB Bound/Classic IRB - No width or Torque/findErrorScaled.m
@@ -15,6 +15,6 @@ function out = findErrorScaled(P, M, J, v_pre, v_post, scale)
     %Using Impact Law Find Predicted Velocity
     predicted = v_pre + inv(M) * J' * P';
     %Find normalized Error Given observed and predicted velocities 
-    out = (norm(v_post(1:2) - predicted(1:2))/norm(v_post(1:2)))^2 + ... 
-    (norm(v_post(3) - predicted(3))/norm(v_post(3)))^2 / scale;
+    D = diag([1;1;scale]);
+    out = (norm(D * (v_post - predicted))/norm(D * v_post))^2;
 end
