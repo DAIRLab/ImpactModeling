87d5861ff6d2abc3ab7e241aad350c376f6e4c1c
andyeske <65673243+andyeske@users.noreply.github.com>
2020-07-22 15:12:59 -0300

    Update createDataSet.m

--- IRB Bound/IRB Square/createDataSet.m
+++ IRB Bound/IRB Square/createDataSet.m
@@ -3,15 +3,18 @@
 trials = 20;
 
 %good trials... data andy and I collected from he visualizers manually
-goodTrials = [];
+goodTrials = [find(trialData(:,1) == 1)];
 
 %loop through trials
-for i = 1:trials
+for k = 1:trials
     %run the read data function to retrieve pre and post impact
     %positions/velocities as well as the normal and tangential Jacobians
     %for the impact
+    
+    i = goodTrials(k);
+     
     [Pre, Post, d, n] = ReadData(i);
-    squareData(i).states = [Pre, Post];
-    squareData(i).n = n;
-    squareData(i).d = d;
+    squareData(k).states = [goodTrials(k), Pre, Post];
+    squareData(k).n = n;
+    squareData(k).d = d;
 end
